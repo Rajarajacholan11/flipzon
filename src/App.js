76efe022fb7d66d3d2e08c7ff05cf8dba14ac122
Rajarajacholan11 <149559911+Rajarajacholan11@users.noreply.github.com>
2025-08-11 10:21:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './COMPONENTS/Header';
 import SlideShow from './COMPONENTS/Slideshow';
 import ProductCards from './COMPONENTS/productscards';
 import Footer from './COMPONENTS/Footer';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
 import ViewProduct from './COMPONENTS/ViewProduct';
-import { useState } from 'react';
 import Cart from './COMPONENTS/Cart';
 import CheckoutPage from './COMPONENTS/CheckoutPage';
 import About from './COMPONENTS/About';
 import Login from './COMPONENTS/Login';
 import Signup from './COMPONENTS/Signup';
 import Allproducts from './COMPONENTS/Allproducts';
+
 function App() {
-  const [cart,setCart]=useState([]);
-  const [cartcount,setCartCount]=useState();
+  const [cart, setCart] = useState([]);
+  const [cartcount, setCartCount] = useState();
+
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <Routes>
-            <Route path="/View-Product/:id" element={<ViewProduct cart={cart} setCart={setCart}/>}/>
-            <Route path='/Cart' element={<Cart cart={cart} />}/>
-            <Route path="/CheckOutPage" element={ <CheckoutPage cart={cart} setCart={setCart} />} />
-            <Route path='/About'  element={<About cart={cart}/>}/>
-            <Route path='/Login' element={<Login cart={cart}/>}/>
-            <Route path='/SignUp' element={<Signup cart={cart}/>}/>
-            <Route path='/Allproducts' element={<Allproducts cart={cart}/>}/>
-
-
-            <Route path="/" element={ 
-              <div className="App">
-                  <div className="div">
-                        {/* <div className='sm:px-6 lg-8'>
-                          <Header count={cart.length}/>
-                        </div> */}
-                         
-                        {/* <SlideShow/> */}
-                        <ProductCards/>                                                           
-                        {/* <Footer/> */}
-
-                  </div>
-          
-
+    <HashRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route
+          path="/View-Product/:id"
+          element={<ViewProduct cart={cart} setCart={setCart} />}
+        />
+        <Route path="/Cart" element={<Cart cart={cart} />} />
+        <Route
+          path="/CheckOutPage"
+          element={<CheckoutPage cart={cart} setCart={setCart} />}
+        />
+        <Route path="/About" element={<About cart={cart} />} />
+        <Route path="/Login" element={<Login cart={cart} />} />
+        <Route path="/SignUp" element={<Signup cart={cart} />} />
+        <Route
+          path="/Allproducts"
+          element={<Allproducts cart={cart} />}
+        />
+        <Route
+          path="/"
+          element={
+            <div className="App">
+              <div className="div">
+                {/* <Header count={cart.length} /> */}
+                {/* <SlideShow /> */}
+                <ProductCards />
+                {/* <Footer /> */}
               </div>
-            }
-            />
-          </Routes>
-      
-    </BrowserRouter>
-   
+            </div>
+          }
+        />
+      </Routes>
+    </HashRouter>
   );
 }
 
